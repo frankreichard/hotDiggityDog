a23377d0233a886d6551f75a1dc794745c25db2f
Frank Reichard <frankreichard10@gmail.com>
2019-07-16 12:35:35 -0400

    Initial Commit

---
+++ hotDiggityDog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hotDiggityDog
+//
+//  Created by Reichard, Frank (US - Mechanicsburg Delivery) on 7/16/19.
+//  Copyright © 2019 Frank Reichard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
